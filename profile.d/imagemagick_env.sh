0723267de6e1433e844fd2ff2c9206b0eb74da8e
Queenie Peng <>
2025-03-26 20:51:56 -0500

    Add libheif to imagemagick

--- profile.d/imagemagick_env.sh
+++ profile.d/imagemagick_env.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-# Let runtime know where to find libheif and other codecs
-export LD_LIBRARY_PATH="/app/.heroku/vendor/libheif/lib:$LD_LIBRARY_PATH"
+# Set runtime library paths for both ImageMagick and libheif
+export LD_LIBRARY_PATH="/app/.heroku/vendor/imagemagick/lib:/app/.heroku/vendor/libheif/lib:$LD_LIBRARY_PATH"
+
+# Set binary path so convert/magick tools are used from custom install
 export PATH="/app/.heroku/vendor/imagemagick/bin:$PATH"
