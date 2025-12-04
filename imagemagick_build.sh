105ca00a706d7b565beb7b6db4089ebb76fb9acd
Queenie Peng <11747477+QueeniePeng@users.noreply.github.com>
2025-12-04 10:55:08 -0600

    Update imagemagick_build.sh with new url

--- imagemagick_build.sh
+++ imagemagick_build.sh
@@ -9,7 +9,7 @@ rm -rf "$IM_INSTALL_DIR"
 mkdir -p "$IM_INSTALL_DIR"
 
 # Download precompiled ImageMagick from GitHub Release
-curl -L -o imagemagick.tar.gz "https://github.com/BigGreenCompany/receipt-ai/releases/download/imagemagick-7.1.1-46-heroku24/ImageMagick-heroku24-7.1.1-46.tar.gz"
+curl -L -o imagemagick.tar.gz "https://github.com/BigGreenCompany/heroku-buildpack-libheif/releases/download/imagemagick-7.1.1-46-heroku24/ImageMagick-heroku24-7.1.1-46.tar.gz"
 
 # Extract into Heroku vendor directory
 tar -xzf imagemagick.tar.gz -C "$IM_INSTALL_DIR"
